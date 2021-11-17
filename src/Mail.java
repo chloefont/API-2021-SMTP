aa2ad998603acf555e83c5e12ddb0f88ccc7ca1c
Luca Coduri <coduri.vitor@gmail.com>
2021-11-17 11:19:47 +0100

    Mail

--- src/Mail.java
+++ src/Mail.java
@@ -1,2 +1,22 @@
 public class Mail {
+    private String[] to;
+    private String from;
+    private String subject;
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String[] getTo() {
+        return to;
+    }
 }
