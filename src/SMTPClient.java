330109f3e5eb706acd15e3d648924f0cf1e855c6
chloefont <cfontaine@citycable.ch>
2021-11-17 11:18:50 +0100

    SMTPClient

--- src/SMTPClient.java
+++ src/SMTPClient.java
@@ -1,2 +1,13 @@
 public class SMTPClient {
+    private String ip;
+    private int port;
+
+    public SMTPClient(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public boolean sendMail(Mail mail) {
+        return true;
+    }
 }
