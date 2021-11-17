dcce419227dc4a233374c767249477624dc1d269
chloefont <cfontaine@citycable.ch>
2021-11-17 11:08:03 +0100

    Person

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,20 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Group group = null;
+
+    Person(String firstName, String lastName, Group group) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
 }
