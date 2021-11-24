9c432c7d151de6de388b3f97514d8e04e90b72bd
Luca Coduri <coduri.vitor@gmail.com>
2021-11-24 14:48:44 +0100

    c'est ok

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,18 @@ public class Main {
         int serverPort = 2525;
 
         SMTPClient smtpClient = new SMTPClient(serverHost, serverPort);
-        String[] tos = {"luca.coduri@heig-vd.ch", "pouetpouet@gmail.com"};
-        Mail mail = new Mail(tos, "chloe.fontaine@heig-vd.ch", "wesh", "jme demandais si sur l'avenue on se retrouverait.");
+        //String[] tos = {"luca.coduri@heig-vd.ch", "pouetpouet@gmail.com"};
+        //Mail mail = new Mail(tos, "chloe.fontaine@heig-vd.ch", "wesh", "jme demandais si sur l'avenue on se retrouverait.");
 
         //smtpClient.sendMail(mail);
         try{
             ConfigurationManager cm = new ConfigurationManager("victims.txt", "pranks.txt");
             //System.out.println(Arrays.toString(cm.getPersons()));
             //System.out.println(Arrays.toString(cm.getPranks()));
-            System.out.println((Arrays.toString((new PrankGenerator()).generateMails(cm.getPersons(),2, cm.getPranks()))));
+            Mail[] mails = (new PrankGenerator()).generateMails(cm.getPersons(),2, cm.getPranks());
+            for(Mail mail : mails){
+                smtpClient.sendMail(mail);
+            }
 
         }catch(IOException exception){
             System.out.println(exception.getMessage());
