3ddb4e7ca1c242ebc3147268ee81554679453aeb
chloefont <cfontaine@citycable.ch>
2021-11-24 11:21:16 +0100

    send mails ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        String serverHost = "smtp.mailtrap.io";
+        int serverPort = 2525;
 
+        SMTPClient smtpClient = new SMTPClient(serverHost, serverPort);
+        String[] tos = {"luca.coduri@heig-vd.ch", "pouetpouet@gmail.com"};
+        Mail mail = new Mail(tos, "chloe.fontaine@heig-vd.ch", "wesh", "jme demandais si sur l'avenue on se retrouverait.");
+
+        smtpClient.sendMail(mail);
     }
 }
